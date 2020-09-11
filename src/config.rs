4ccebd8de27f9dba15a226b45e65110e152c6992
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-09-10 23:58:55 -0500

    building config file manager, will remove test files

--- src/config.rs
+++ src/config.rs
@@ -1,8 +1,12 @@
 extern crate config;
 
+use std::path::Path;
+use std::io::Write;
+use std::fs::OpenOptions;
+
 use std::collections::HashMap;
 
-pub fn test_config() {
+pub fn read_config() -> HashMap<String, String> {
     let mut settings = config::Config::default();
     settings
         // Add in `./Settings.toml`
@@ -11,7 +15,31 @@ pub fn test_config() {
         // Eg.. `APP_DEBUG=1 ./target/app` would set the `debug` key
         .merge(config::Environment::with_prefix("APP")).unwrap();
 
-    // Print out our settings (as a HashMap)
-    println!("{:?}",
-             settings.try_into::<HashMap<String, String>>().unwrap());
+    let settings_hashmap = settings.try_into::<HashMap<String, String>>().unwrap();
+    
+    save(&settings_hashmap);
+    
+    settings_hashmap
+}
+
+pub fn save(config: &HashMap<String, String>) {
+    let path = Path::new("test.toml");
+
+    // sort hashmap into an alphabetically ordered vector
+    let mut sorted_config: Vec<_> = config.into_iter().collect();
+    sorted_config.sort_by(|x,y| x.0.cmp(&y.0));
+    
+    if let Ok(mut file) = OpenOptions::new().write(true).open(path) {
+        file.set_len(0).unwrap();
+        for (key, value) in sorted_config {
+            //TODO: find library, or manually add nested HashMap support along with list support
+            file.write_all(
+                format!(
+                    "{0}=\"{1}\"\n",
+                    key,
+                    value
+                ).as_bytes()
+            ).unwrap();
+        }
+    }
 }
--- src/main.rs
+++ src/main.rs
@@ -4,31 +4,12 @@ mod oshelper;
 
 
 fn main() {
-//     let matches = options::get_matches();
+    let matches = options::get_matches();
 
-     // config::test_config();
-     let pathbuf = oshelper::combine_paths(&["hell0","hi", "dev", "bruh","rust","is","hard"]);
-     let test = pathbuf.into_os_string().into_string().unwrap();
-     println!("{}",test);
+    config::read_config();
+    // let pathbuf = oshelper::combine_paths(&["hell0","hi", "dev", "bruh","rust","is","hard"]);
+    // let test = pathbuf.into_os_string().into_string().unwrap();
+    // println!("{}",test);
 
     return;
-//     // Gets a value for config if supplied by user, or defaults to "default.conf"
-//     let config = matches.value_of("config").unwrap_or("default.conf");
-//     println!("Value for config: {0}", config);
-
-//     // Calling .unwrap() is safe here because "INPUT" is required (if "INPUT" wasn't
-//     // required we could have used an 'if let' to conditionally get the value)
-//     println!("Using input file: {0}", matches.value_of("INPUT").unwrap());
-
-//     // Vary the output based on how many times the user used the "verbose" flag
-//     // (i.e. 'myprog -v -v -v' or 'myprog -vvv' vs 'myprog -v'
-//     match matches.occurrences_of("v") {
-//         0 => println!("No verbose info"),
-//         1 => println!("Some verbose info"),
-//         2 => println!("Tons of verbose info"),
-//         3 | _ => println!("Don't be crazy"),
-//     }
-
-
-    // more program logic goes here...
 }
