ed2f03bcdc66fed8d96c7cf881ac03ea3f530ba7
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-09-13 19:50:19 -0500

    adding more path information

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
+extern crate dirs;
+
 mod options;
 mod config;
 mod oshelper;
-
 
 fn main() {
     let matches = options::get_matches();
@@ -10,6 +11,16 @@ fn main() {
     // let pathbuf = oshelper::combine_paths(&["hell0","hi", "dev", "bruh","rust","is","hard"]);
     // let test = pathbuf.into_os_string().into_string().unwrap();
     // println!("{}",test);
+
+    match dirs::config_dir() {
+        Some(path) => 
+        {
+            println!("Your home directory, probably: {}", path.display());
+            let test = oshelper::combine_paths(&[&path.to_str().unwrap(), "prox"]);
+            println!("{:?}", test.to_str().unwrap());
+        }
+        None => println!("Impossible to get your home dir!"),
+    }
 
     return;
 }
