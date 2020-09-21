8b08430bb50588df412a6f20e55f92a632571932
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-09-20 23:36:54 -0500

    adding basic password management

--- prox-client/src/defaults.rs
+++ prox-client/src/defaults.rs
@@ -5,9 +5,9 @@ use std::collections::HashMap;
 pub fn default_values() -> HashMap<String, String>
 {
     let mut values: HashMap<String, String> = HashMap::new();
-    values.insert("key".to_string(),"Random_key".to_string());
-    values.insert("debug".to_string(),"false".to_string());
-    values.insert("cache".to_string(),"true".to_string());
+    values.insert("server_id".to_string(),"prox-server".to_string());
+    // values.insert("debug".to_string(),"false".to_string());
+    // values.insert("cache".to_string(),"true".to_string());
     
     values
 }
--- prox-client/src/main.rs
+++ prox-client/src/main.rs
@@ -1,5 +1,8 @@
+#![allow(unused)]
+
 extern crate dirs;
 extern crate rpassword;
+extern crate keyring;
 
 mod options;
 mod config;
@@ -11,16 +14,32 @@ extern crate rand;
 use rand::Rng; 
 use rand::distributions::Alphanumeric;
 
+static LANGUAGE: &str = "Rust";
+const THRESHOLD: i32 = 10;
+
+// static USERNAME: &str = "PROX-CLIENT";
+static APP_NAME: &str = "prox";
+static EXTENSION: &str = ".toml";
+
+
+
+
 fn main() {
-    let _matches = options::get_matches();
-    let app_config = config::AppConfig::new("prox".to_string(), ".toml".to_string(), defaults::default_values());
-    
-    println!("{:?}",app_config.read_config());
+    let matches = options::get_matches();
 
+    let app_config = config::AppConfig::new(APP_NAME.to_string(), EXTENSION.to_string(), defaults::default_values());
+    let app_settings = app_config.read_config();
 
-    // println!("Type a password: ");
-    // let password = read_password().unwrap();
-    // println!("The password: {}", password);
+    let server_id = app_settings.get("server_id").unwrap();
+
+    if matches.args.contains_key("deletecache")
+    {
+        println!("Deleting cached data ...");
+        delete_password(&server_id);
+        return;
+    }
+
+    println!("{:?}", &app_settings);
 
     let key = rand::thread_rng()
     .sample_iter(&Alphanumeric)
@@ -28,5 +47,42 @@ fn main() {
     .collect::<String>();
     println!("{}",key);
 
+    println!("password: {}", get_password(&server_id));
+
     return;
+}
+
+fn get_password(username: &str) -> String
+{
+    let service = "rust-keyring";
+    let keyring = keyring::Keyring::new(&service, &username);
+
+    let password = match keyring.get_password()
+    {
+        Ok(passwd) => passwd, 
+        Err(_e) => set_new_password(&username),
+    };
+
+    password
+}
+
+fn set_new_password(username: &str) -> String
+{
+    let service = "rust-keyring";
+    let keyring = keyring::Keyring::new(&service, &username);
+
+    println!("Type the password for {0} (input hidden): ", &username);
+    let password = read_password().unwrap();
+
+    keyring.set_password(&password);
+
+    password
+}
+
+fn delete_password(username: &str)
+{
+    let service = "rust-keyring";
+    let keyring = keyring::Keyring::new(&service, &username);
+
+    keyring.delete_password();
 }
