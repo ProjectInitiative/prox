b7d1eb349d64b8478353114d998a9bf9fa676f31
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-11-01 14:14:41 -0600

    adding some basic rest api style server control with rocket.rs

--- prox-server/src/main.rs
+++ prox-server/src/main.rs
@@ -1,3 +1,7 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
+#[macro_use] extern crate rocket;
+
 extern crate dirs;
 extern crate rpassword;
 
@@ -11,22 +15,29 @@ extern crate rand;
 use rand::Rng; 
 use rand::distributions::Alphanumeric;
 
+#[get("/")]
+fn index() -> &'static str 
+{
+    "hello, world!"
+}
+
 fn main() {
     let _matches = options::get_matches();
     let app_config = config::AppConfig::new("prox".to_string(), ".toml".to_string(), defaults::default_values());
     
     println!("{:?}",app_config.read_config());
 
-
+    
     // println!("Type a password: ");
     // let password = read_password().unwrap();
     // println!("The password: {}", password);
-
     let key = rand::thread_rng()
-    .sample_iter(&Alphanumeric)
-    .take(20)
-    .collect::<String>();
+            .sample_iter(&Alphanumeric)
+            .take(20)
+            .collect::<String>();
     println!("{}",key);
-
+    
+    rocket::ignite().mount("/", routes![index]).launch();
+    
     return;
 }
