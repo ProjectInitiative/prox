d13b80f0011c0359babf54b03388035d5b4f33db
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-11-08 11:41:11 -0600

    adding basic json parsing and Arc/Mutexed Vector structures

--- prox-server/src/main.rs
+++ prox-server/src/main.rs
@@ -15,10 +15,47 @@ extern crate rand;
 use rand::Rng; 
 use rand::distributions::Alphanumeric;
 
-#[get("/")]
-fn index() -> &'static str 
+use std::{thread, time};
+use std::sync::{Arc, Mutex};
+use rocket::{response::content, State};
+
+use serde::{Serialize, Deserialize};
+
+static SOME_INT: i32 = 5;
+
+static BASE_URL: &str = "/api/v1";
+
+#[derive(Serialize, Deserialize)]
+struct VMInfo
 {
-    "hello, world!"
+    vm_queue: Vec<i32>,
+}
+
+#[get("/")]
+fn index() -> String
+{
+    format!("Hello!\nOne day I will add a nice UI or instructions to setup and view via a web interface!")
+}
+
+#[get("/vms-in-queue")]
+fn vms_in_queue(queue: State<Arc<Mutex<Vec<i32>>>>) -> content::Json<String>
+{   
+    let vm_info = VMInfo { vm_queue: queue.lock().unwrap().clone() };
+    let serialized_vm_info = 
+    queue.lock().unwrap().push(10);
+    // let json = format!("{{ \"hi\": \"{0}\" }}",queue.lock().unwrap().last().unwrap().to_string());
+    // let x = &json[..];
+    let y = format!("{{ \"hi\": \"{0}\" }}",queue.lock().unwrap().last().unwrap()).as_str();
+    // content::Json(format!("{{ \"hi\": \"{0}\" }}",queue.lock().unwrap().last().unwrap()))
+    // content::Json(y)
+    content::Json(format!("{{ \"hi\": \"{0}\" }}",queue.lock().unwrap().last().unwrap()))
+}
+
+
+#[post("/add-vm-to-queue")]
+fn add_vm_to_queue()
+{
+
 }
 
 fn main() {
@@ -27,7 +64,8 @@ fn main() {
     
     println!("{:?}",app_config.read_config());
 
-    
+    let vm_queue = Arc::new(Mutex::new(Vec::<i32>::new()));
+
     // println!("Type a password: ");
     // let password = read_password().unwrap();
     // println!("The password: {}", password);
@@ -36,8 +74,17 @@ fn main() {
             .take(20)
             .collect::<String>();
     println!("{}",key);
+
+    thread::spawn(move || {
+        // some work here
+    });
     
-    rocket::ignite().mount("/", routes![index]).launch();
+    rocket::ignite()
+        .manage(vm_queue)
+        .mount("/", routes![index])
+        .mount(BASE_URL, routes![vms_in_queue, add_vm_to_queue])
+        .launch();
+    
     
     return;
 }
