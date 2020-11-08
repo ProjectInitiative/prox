8df1ef7e4a0fe25157ee8f6c13466cf4ec4f4e1a
Kyle Petryszak <6314611+ProjectInitiative@users.noreply.github.com>
2020-11-08 12:58:10 -0600

    added basic queuing with REST APIs

--- prox-server/src/main.rs
+++ prox-server/src/main.rs
@@ -17,18 +17,54 @@ use rand::distributions::Alphanumeric;
 
 use std::{thread, time};
 use std::sync::{Arc, Mutex};
+
+use std::io::Read;
 use rocket::{response::content, State};
+use rocket::{Request, Data, Outcome, Outcome::*};
+use rocket::data::{self, FromDataSimple};
+use rocket::http::{Status, ContentType};
 
 use serde::{Serialize, Deserialize};
 
 static SOME_INT: i32 = 5;
 
 static BASE_URL: &str = "/api/v1";
+const LIMIT: u64 = 256;
 
 #[derive(Serialize, Deserialize)]
+struct VMInfoList
+{
+    vm_queue: Vec<VMInfo>,
+}
+
+#[derive(Clone, Serialize, Deserialize)]
 struct VMInfo
 {
-    vm_queue: Vec<i32>,
+    title: String,
+    description: String,
+    id: i32,
+}
+
+impl FromDataSimple for VMInfo
+{
+    type Error = String;
+
+    fn from_data(req: &Request, data: Data) -> data::Outcome<Self, String> {
+        // Ensure the content type is correct before opening the data.
+        let vm_info_ct = ContentType::new("application", "json");
+        if req.content_type() != Some(&vm_info_ct) {
+            return Outcome::Forward(data);
+        }
+
+        // Read the data into a String.
+        let mut string = String::new();
+        if let Err(e) = data.open().take(LIMIT).read_to_string(&mut string) {
+            return Failure((Status::InternalServerError, format!("{:?}", e)));
+        }
+        
+        // add error checking for the JSON deserialization
+        Success(serde_json::from_str(&string).unwrap())
+    }
 }
 
 #[get("/")]
@@ -38,24 +74,26 @@ fn index() -> String
 }
 
 #[get("/vms-in-queue")]
-fn vms_in_queue(queue: State<Arc<Mutex<Vec<i32>>>>) -> content::Json<String>
+fn vms_in_queue(queue: State<Arc<Mutex<Vec<VMInfo>>>>) -> content::Json<String>
 {   
-    let vm_info = VMInfo { vm_queue: queue.lock().unwrap().clone() };
-    let serialized_vm_info = 
-    queue.lock().unwrap().push(10);
-    // let json = format!("{{ \"hi\": \"{0}\" }}",queue.lock().unwrap().last().unwrap().to_string());
-    // let x = &json[..];
-    let y = format!("{{ \"hi\": \"{0}\" }}",queue.lock().unwrap().last().unwrap()).as_str();
-    // content::Json(format!("{{ \"hi\": \"{0}\" }}",queue.lock().unwrap().last().unwrap()))
-    // content::Json(y)
-    content::Json(format!("{{ \"hi\": \"{0}\" }}",queue.lock().unwrap().last().unwrap()))
+    let vm_info = VMInfoList { vm_queue: queue.lock().unwrap().clone() };
+    content::Json(serde_json::to_string(&vm_info).unwrap())
 }
 
 
-#[post("/add-vm-to-queue")]
-fn add_vm_to_queue()
+#[post("/add-vm-to-queue", format = "application/json", data = "<vm_to_add>")]
+fn add_vm_to_queue(queue: State<Arc<Mutex<Vec<VMInfo>>>>, vm_to_add: VMInfo) -> content::Json<String>
+{
+    // println!("hello");
+    queue.lock().unwrap().push(vm_to_add);
+    content::Json("".to_string())
+}
+
+#[delete("/delete-vm-from-queue", format = "application/json", data = "<vm_to_delete>")]
+fn delete_vm_from_queue(vm_to_delete: VMInfo) -> content::Json<String>
 {
 
+    content::Json("".to_string())
 }
 
 fn main() {
@@ -64,7 +102,7 @@ fn main() {
     
     println!("{:?}",app_config.read_config());
 
-    let vm_queue = Arc::new(Mutex::new(Vec::<i32>::new()));
+    let vm_queue = Arc::new(Mutex::new(Vec::<VMInfo>::new()));
 
     // println!("Type a password: ");
     // let password = read_password().unwrap();
